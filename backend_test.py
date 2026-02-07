ab7d8907245420643f788b13c7f13ff68f813a24
Festive Lights Dev <dev@festivelights.com>
2026-02-07 07:22:42 +0000

    auto-commit for 9e499ade-cd49-4bb6-80db-c378b1fdaf7d

--- backend_test.py
+++ backend_test.py
@@ -11,7 +11,7 @@ from datetime import datetime, timedelta
 from typing import Dict, Any, Optional
 
 class FestiveCRMTester:
-    def __init__(self, base_url: str = "https://xmas-light-mgr.preview.emergentagent.com"):
+    def __init__(self, base_url: str = "https://festivelights.preview.emergentagent.com"):
         self.base_url = base_url
         self.api_url = f"{base_url}/api"
         self.token = None
